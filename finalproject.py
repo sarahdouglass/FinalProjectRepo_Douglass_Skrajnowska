20b2b6296b669945c27958000f63def7f6234489
Sarah <douglass.s@northeastern.edu>
2024-03-19 16:49:43 -0400

    Adding decorators as needed, adding more methods under both simple and compex

--- finalproject.py
+++ finalproject.py
@@ -1,21 +1,36 @@
 #Final Project
 
+class Calculator: #(for graphics)
+    pass
+
+
 class Simple_Operations:
    
+    @staticmethod
     def addition(number1,number2):
         return number1 + number2
     
+    @staticmethod
     def subtraction(number1,number2):
         return number1 - number2
     
+    @staticmethod
     def multiplication(number1,number2):
         return number1 * number2
     
+    @staticmethod
     def division(number1,number2):
         return number1 / number2
 
 class Complex_Operations(Simple_Operations):
     
+    @staticmethod
     def exponent(number1,number2):
         return number1 ** number2
-   
+    
+    @staticmethod
+    def square(number1):
+        return number1**0.5
+    
+    @staticmethod
+   def trig
