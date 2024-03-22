55e68094b19565a335f7f20b6a6d2b0cc3772f87
zoskr <skrajnowskaz@gmail.com>
2024-03-22 14:49:29 -0400

    New file to learn graphics, buttons configured without functions, label for calculations made

--- GraphicsPrac.py
+++ GraphicsPrac.py
@@ -6,10 +6,9 @@ class App:
         self.root = tk.Tk()
         self.root.title('Calculator')
         
-        self.box = Label("CALC")
+        self.box = Label("CALCULATING STUFF")
         
         self.funcs = 'c^%/789*456-123+ 0.='
-        #self.funcs = [['c','^','%','/'],[7,8,9,'*'],[4,5,6,'-'],[1,2,3,'+'],['',0,'.','=']]       
         for i, b in enumerate(self.funcs):
             Button(b, row = i//4+1, column = i%4)
         
@@ -25,7 +24,7 @@ class Button(ttk.Button):
 class Label(tk.Label):
     def __init__(self, text, **kwargs):
         super().__init__(text = text, relief = 'solid', **kwargs)
-        self.grid()
+        self.grid(columnspan = 4)
 
 
 
