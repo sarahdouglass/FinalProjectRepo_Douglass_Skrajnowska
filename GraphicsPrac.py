28bb583db17b20086d996bb3e2d21233031a4cad
zoskr <skrajnowskaz@gmail.com>
2024-03-24 14:53:27 -0400

    Buttons are bound to single function which changes the label

--- GraphicsPrac.py
+++ GraphicsPrac.py
@@ -6,9 +6,11 @@ class App:
         self.root = tk.Tk()
         self.root.title('Calculator')
         
-        self.box = Label("CALCULATING STUFF")
+        self.label_var = tk.StringVar(value="CALCULATING")
+        self.box = Label(textvariable=self.label_var)
         
         self.funcs = 'c^%/789*456-123+ 0.='
+        self.funcs = "0"
         for i, b in enumerate(self.funcs):
             Button(b, row = i//4+1, column = i%4)
         
@@ -17,19 +19,24 @@ class App:
         
 class Button(ttk.Button):
     def __init__(self, text, row, column, **kwargs):
-        super().__init__(text = text, **kwargs)
-        self.grid(row = row,column = column)
-
+        super().__init__(text=text, command=self.counter, **kwargs)
+        self.grid(row = row, column = column)
+        
+    def counter(self):
+        global app
+        app.label_var.set("Updates")
         
 class Label(tk.Label):
-    def __init__(self, text, **kwargs):
-        super().__init__(text = text, relief = 'solid', **kwargs)
+    def __init__(self, **kwargs):
+        global app
+        super().__init__(relief='solid', **kwargs)
         self.grid(columnspan = 4)
 
 
 
-
 def main():
-    calc = App()
-    calc.run()
+    global app
+    app = App()
+    app.run()
+
 main()
