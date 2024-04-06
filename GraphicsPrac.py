c0e4426d2d811fc024daa5f267268dfec9f13868
zoskr <skrajnowskaz@gmail.com>
2024-04-06 12:05:01 -0400

    Added key binds on practice file to learn how to they work

--- GraphicsPrac.py
+++ GraphicsPrac.py
@@ -10,11 +10,25 @@ class App:
         self.box = Label(textvariable=self.label_var)
         
         self.funcs = 'c^%/789*456-123+ 0.='
+        self.buttons = {}
         for i, b in enumerate(self.funcs):
-            Button(b, row = i//4+1, column = i%4)
+            self.buttons.update({b:Button(b, row = i//4+1, column = i%4)})
+        
+        self.keyBind()
         
     def run(self):
+        print(self.buttons)
         self.root.mainloop()
+        
+    def keyBind(self):
+        #keys = self.buttons.keys()
+        for i in range(10):
+            self.root.bind(str(i), self.key_press)
+            
+    def key_press(self,a):
+        x = str(self.label_var.get()) + str(self.text)
+        self.label_var.set(x)
+        
         
 class Button(ttk.Button):
     def __init__(self, text, row, column, **kwargs):
@@ -22,11 +36,16 @@ class Button(ttk.Button):
         self.text = text
         self.grid(row = row, column = column)
         
+        
     def counter(self):
         global app
         x = str(app.label_var.get()) + str(self.text)
         app.label_var.set(x)
-        
+    '''    
+    def keyBind(self):
+        global app
+        x = str(app.label_var.get()) + str(self.text)
+  '''      
 class Label(tk.Label):
     def __init__(self, **kwargs):
         global app
