6b90202f8972fcaca3a47a1dd649bbe26749bd84
zoskr <skrajnowskaz@gmail.com>
2024-04-07 19:38:03 -0400

    Added back all functions

--- finalproject.py
+++ finalproject.py
@@ -1,65 +1,74 @@
-# COMMENTS TO BE ADDED AFTER CODE STARTS WORKING
-#Trig and logs still dont work
-
 import tkinter as tk
 import tkinter.ttk as ttk
 import numpy as np
+import math as m
 
-
+# Define the main application class
 class App:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title('Calculator')
 
+        # Variable to hold the value displayed on the calculator
         self.label_var = tk.StringVar(value="0")
         self.box = Label(textvariable=self.label_var)
 
-        self.funcs = [('c', 'C'), ('^', '^'), ('%', '%'), ('L', ' '),
-                      ('S', ' '), ('C', ' '), ('T', ' '), ('/', '/'),
+        # Define the buttons and their corresponding key bindings
+        self.funcs = [('c', 'C'), ('^', '^'), ('s', ' '), ('(', '('),
+                      ('S', ' '), ('C', ' '), ('T', ' '), (')', ')'),
+                      ('L', ' '), ('l', ' '), ('F', '!'), ('/', '/'),
                       ('7', '7'), ('8', '8'), ('9', '9'), ('*', '*'),
                       ('4', '4'), ('5', '5'), ('6', '6'), ('-', '-'),
                       ('1', '1'), ('2', '2'), ('3', '3'), ('+', '+'),
-                      ('del', '<BackSpace>'), ('0', '0'), ('.', '.'), ('=', '<Return>')]
-        
-        self.buttons = {}
-        for i, (b, key) in enumerate(self.funcs):
-           if b == "c":
-              self.buttons[b] = ClearButton(b, row=i//4+1, column=i%4)
-              self.root.bind(key, self.key_press)
-           elif b == 'del':
-               self.buttons[b] = DelButton(b, row=i//4+1, column=i%4)
-               self.root.bind(key, self.key_press)
-           elif b == "=":
-               self.buttons[b] = EqualsButton(b, row=i//4+1, column=i%4)
-               self.root.bind(key, self.key_press)
-           else:
-               if b == "S":
-                   x = "sin"
-               elif b == "C":
-                   x = "cos"
-               elif b == "T":
-                   x = "tan"
-               elif b == "L":
-                   x = "log"
-               else:
-                   x = b
-               self.buttons[b] = Button(x, row=i//4+1, column=i%4)
-               
-               if(key != ' '):
-                   self.root.bind(key, self.key_press)
+                      ('d', '<BackSpace>'), ('0', '0'), ('.', '.'), ('=', '<Return>')]
 
+        # Dictionary to hold the buttons
+        self.buttons = {}
+
+        # Create buttons and bind key presses
+        for i, (b, key) in enumerate(self.funcs):
+            if b == "c":
+                self.buttons[b] = ClearButton(row=i//4+1, column=i%4)
+                self.root.bind(key, self.key_press)
+            elif b == 'd':
+                self.buttons[b] = DelButton(row=i//4+1, column=i%4)
+                self.root.bind(key, self.key_press)
+            elif b == "=":
+                self.buttons[b] = EqualsButton(b, row=i//4+1, column=i%4)
+                self.root.bind(key, self.key_press)
+            elif b.isalpha() == False:
+                self.buttons[b] = Button(b, row=i//4+1, column=i%4)
+                self.root.bind(key, self.key_press)
+            else:
+                if b == "S":
+                    x = "sin"
+                elif b == "C":
+                    x = "cos"
+                elif b == "T":
+                    x = "tan"
+                elif b == "s":
+                    x = "sqrt"
+                elif b == "L":
+                    x = "ln"
+                elif b == "l":
+                    x = "log"
+                elif b == "F":
+                    x = "!"
+                self.buttons[b] = FuncButton(x, row=i//4+1, column=i%4)
+                if(key != ' '):
+                    self.root.bind(key, self.key_press)
+            
+
+    # Run the application
     def run(self):
-        print(self.buttons.keys())
         self.root.mainloop()
-        
-        
+
+    # Handle key press events
     def key_press(self, event):
         if event.keysym == 'C':
             self.label_var.set('0')
         elif event.keysym == '^':
             self.label_var.set(self.label_var.get() + '^')
-        elif event.keysym == '%':
-            self.label_var.set(self.label_var.get() + '%')
         elif event.keysym == '/':
             self.label_var.set(self.label_var.get() + '/')
         elif event.keysym == '*':
@@ -73,19 +82,18 @@ class App:
         elif event.keysym == 'Space':
             self.label_var.set(self.label_var.get() + ' ')
         elif event.keysym == 'BackSpace':
-            eq = self.label_var.get()
-            if eq != "0":
-                if eq[-1].isalpha():
-                    x = str(eq)[:-3]
-                else:
-                    x = str(eq)[:-1] 
-            if x == "":
-                x = "0"
-            self.label_var.set(str(x))   
+            if app.label_var.get() != "0":
+                x = str(app.label_var.get())[:-1]
+                app.label_var.set(x)
         elif event.keysym == 'Return':
             self.calculate()
+        elif event.keysym == '(':
+            self.label_var.set(self.label_var.get() + '(')
+        elif event.keysym == ')':
+            self.label_var.set(self.label_var.get() + ')')
         else:
             self.label_var.set(self.label_var.get() + event.char)
+
 
 #=============================================================================
 
@@ -105,35 +113,46 @@ class Button(ttk.Button):
 
 #=============================================================================
 
-class ClearButton(Button):
+class FuncButton(Button):
     def __init__(self, text, row, column):
+        super().__init__(text, row, column)
+
+    def counter(self):
+        global app
+        if app.label_var.get() == "0":
+            x = self.text + "("
+        else:
+            x = str(app.label_var.get()) + str(self.text) + "("
+        app.label_var.set(x)
+        
+#=============================================================================
+
+class ClearButton(Button):
+    def __init__(self, row, column):
+        text = "clear"
         super().__init__(text, row, column)
 
     def counter(self):
         global app
         x = "0"
         app.label_var.set(x)
-
+        
 #=============================================================================
-
 
 class DelButton(Button):
-    def __init__(self, text, row, column):
+    def __init__(self, row, column):
+        text = "del"
         super().__init__(text, row, column)
     def counter(self):
-       global app
-       eq = app.label_var.get()
-       if eq != "0":
-           if eq[-1].isalpha():
-               x = str(eq)[:-3]
-           else:
-               x = str(eq)[:-1]    
-       if x == "":
-           x = "0"
-       app.label_var.set(str(x))   
-            
+        global app
+        if app.label_var.get() == "0":
+            x = app.label_var.get()
+        else:
+            x = str(app.label_var.get())[:-1]
+            app.label_var.set(x)
+
 #=============================================================================
- 
+
 class EqualsButton(Button):
     def __init__(self, text, row, column):
         super().__init__(text, row, column)
@@ -155,51 +174,70 @@ def isFloat(equation):
 
 def calculating(equation):
     op = Simple_Operations()
-    
+    i = 0
     while not isFloat(equation):
-        funcs = ("sin", "cos", "tan", "log")
-        for func in funcs:
-            if func in equation:
-                start = equation.index(func)
-                end = start + len(func)
-                
-                if equation[start - 1].isalnum():  # so you can multiply without putting * (only applies for functions)
-                    equation = equation[:start] + "*" + equation[start:]
+        
+        if "!" in equation:
+            op_index = equation.index("!")
+            leftSide = ""
+            
+            i = op_index - 1
+            while i >= 0 and equation[i].isdigit():
+                leftSide = equation[i] + leftSide
+                i -= 1
+            
+            result = op.factorial(float(leftSide))
+            equation = str(equation[:op_index - len(leftSide)]) + str(result) + equation[op_index+1:]
 
-                num = ""
-                i = end
-                while i < len(equation) and (equation[i].isdigit() or equation[i] == '.'):
-                    num += equation[i]
-                    i += 1
+        elif "(" in equation:
+            start_index = equation.index("(")
+            end_index = start_index + equation[start_index:].index(")")
+            sub_eq = calculating(equation[start_index + 1:end_index])
 
-                if func == "sin":
-                    result = op.sin(float(num))
-                elif func == "cos":
-                    result = op.cos(float(num))
-                elif func == "tan":
-                    result = op.tan(float(num))
-                elif func == "log":
-                    result = op.log(float(num))
-
-                equation = equation[:start] + str(result) + equation[i:]
-
-        if "^" in equation:
+            if isFloat(sub_eq):
+                if start_index - 4 < 0:
+                    start = 0
+                else:
+                    start = start_index - 4
+                func = equation[start:start_index]
+                if len(func) > 3:
+                    if func[0].isalpha() or func[1].isalpha() or func[2].isalpha() or func[3].isalpha() or func[4].isalpha():
+                        num = float(sub_eq)
+                        if "sin" in func:
+                            equation = equation.replace(equation[start_index - 3:end_index + 1], str(m.sin(num)))
+                        elif "cos" in func:
+                            equation = equation.replace(equation[start_index - 3:end_index + 1], str(m.cos(num)))
+                        elif "tan" in func:
+                            equation = equation.replace(equation[start_index - 3:end_index + 1], str(m.tan(num)))
+                        elif "log" in func:
+                            equation = equation.replace(equation[start_index - 3:end_index + 1], str(m.log(num)))
+                        elif "ln" in func:
+                            equation = equation.replace(equation[start_index - 2:end_index + 1], str(op.natlog(num)))
+                        elif "sqrt" in func:
+                            equation = equation.replace(equation[start_index - 4:end_index + 1], str(op.square_root(num)))
+                    else:
+                        equation = equation[:start_index] + str(calculating(sub_eq)) + equation[end_index + 1:]
+                else:
+                    equation = equation[:start_index] + str(calculating(sub_eq)) + equation[end_index + 1:]
+            else:
+                equation = equation[:start_index] + str(calculating(sub_eq)) + equation[end_index + 1:]
+        elif "^" in equation:
             op_index = equation.index("^")
 
-            left_side = ""
-            right_side = ""
+            leftSide = ""
+            rightSide = ""
 
             i = op_index - 1
             while i >= 0 and equation[i].isdigit():
-                left_side = equation[i] + left_side
+                leftSide = equation[i] + leftSide
                 i -= 1
             start = i
             i = op_index + 1
-            while i < len(equation) and equation[i].isdigit():
-                right_side += equation[i]
+            while i <= len(equation) - 1 and (equation[i].isdigit() or equation[i] == '.' or equation[i] == ''):
+                rightSide += equation[i]
                 i += 1
 
-            result = op.exponent(float(left_side), float(right_side))
+            result = op.exponent(float(leftSide), float(rightSide))
             equation = str(equation[:start + 1] if start > 0 else '') + str(result) + equation[i:]
         elif "*" in equation or "/" in equation:
             mul_index = equation.find("*")
@@ -212,22 +250,22 @@ def calculating(equation):
                 op_index = min(mul_index, div_index)
 
             operator = equation[op_index]
-            left_side = ""
-            right_side = ""
+            leftSide = ""
+            rightSide = ""
 
             i = op_index - 1
             while i >= 0 and (equation[i].isdigit() or equation[i] == '.' or equation[i] == "-"):
-                left_side = equation[i] + left_side
+                leftSide = equation[i] + leftSide
                 i -= 1
             start = i
             i = op_index + 1
-            while i <= len(equation) - 1 and (equation[i].isdigit() or equation[i] == '.' or equation[i] == "-"):
-                right_side += equation[i]
+            while i <= len(equation) - 1 and (equation[i].isdigit() or equation[i] == '.' or (equation[i] == "-" and equation[i-1].isdigit() != True)):
+                rightSide += equation[i]
                 i += 1
             if operator == "*":
-                result = op.multiplication(float(left_side), float(right_side))
+                result = op.multiplication(float(leftSide), float(rightSide))
             else:
-                result = op.division(float(left_side), float(right_side))
+                result = op.division(float(leftSide), float(rightSide))
             equation = str(equation[:start + 1] if start > 0 else '') + str(result) + equation[i:]
         elif "+" in equation or "-" in equation:
             add_index = equation.find("+")
@@ -241,40 +279,35 @@ def calculating(equation):
             else:
                 op_index = min(add_index, sub_index)
             operator = equation[op_index]
-            left_side = ""
-            right_side = ""
+            leftSide = ""
+            rightSide = ""
 
             i = op_index - 1
             while i >= 0 and (equation[i].isdigit() or equation[i] == "." or equation[i] == "-"):
-                left_side = equation[i] + left_side
+                leftSide = equation[i] + leftSide
                 i -= 1
 
             i = op_index + 1
             while i < len(equation) and (equation[i].isdigit() or equation[i] == "." or equation[i] == "-"):
-                right_side += equation[i]
+                rightSide += equation[i]
                 i += 1
 
             if operator == "+":
-                result = op.addition(float(left_side), float(right_side))
+                result = op.addition(float(leftSide), float(rightSide))
             else:
-                result = op.subtraction(float(left_side), float(right_side))
+                result = op.subtraction(float(leftSide), float(rightSide))
 
-            equation = (equation[:(op_index - len(left_side))]) + str(result) + (equation[i:])
+            equation = (equation[:(op_index - len(leftSide))]) + str(result) + (equation[i:])
             if equation[-2:] == ".0":
                 equation = equation[:-2]
 
     return float(equation)
-
-
-#=============================================================================
 
 class Label(tk.Label):
     def __init__(self, **kwargs):
         global app
         super().__init__(relief='solid', **kwargs)
         self.grid(columnspan=4)
-
-#=============================================================================
 
 class Simple_Operations:
     @staticmethod
@@ -300,47 +333,50 @@ class Simple_Operations:
     def exponent(number1, number2):
         return number1 ** number2
 
+    @staticmethod
+    def square_root(number1):
+        return number1 ** 0.5
 
     @staticmethod
-    def log(number1):
-        if number1 < 0:
-            return np.log10(number1)
-        else: 
-            return "ERROR"
+    def square(number1):
+        return number1 ** 2
 
+    @staticmethod
+    def fact(number1):
+        return np.math.factorial(number1)
+
+    @staticmethod
+    def natlog(number1):
+        return np.log(number1)
+
+    @staticmethod
+    def log10(number1):
+        return np.log10(number1)
+
+    @staticmethod
+    def factorial(number):
+        if number == 0:
+            return 1
+        if number < 0 or number > 10:
+            return "ERROR"
+        else:
+            return number * Simple_Operations.factorial(number - 1)
         
     @staticmethod
     def sin(number1):
-       try:
-           ans = np.sin(number1)
-           return ans
-       except ValueError:
-           return "ERROR"
+       return np.sin(number1)
    
     @staticmethod
     def cos(number1):
-        try:
-            ans = np.cos(number1)
-            return ans
-        except ValueError:
-            return "ERROR"
+        return np.cos(number1)
     
     @staticmethod
     def tan(number1):
-        try:
-            ans = np.tan(number1)
-            return ans
-        except ValueError:
-            return "ERROR"
-
-#=============================================================================
+        return np.tan(number1)
 
 def main():
     global app
     app = App()
     app.run()
 
-
 main()
-
-    
